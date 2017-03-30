afcffaaa61565466abb508e088f35eb8accab81f
Michael Constanza <Michael Constanza>
2017-03-30 10:51:59 -0500

    changed image links

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 console.log('dirname', __dirname)
-app.use(serveStatic(path.join(__dirname, 'watchOver')));
+app.use(serveStatic(__dirname));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
