60e4b313fff2259b3d5ec002ef00c237b9e427d8
Michael Constanza <Michael Constanza>
2017-03-30 10:48:52 -0500

    route settings

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 console.log('dirname', __dirname)
-app.use(serveStatic(__dirname +'/watchOver'));
+app.use(serveStatic(path.join(__dirname, 'watchOver')));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
